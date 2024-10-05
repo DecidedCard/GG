afaf279cfdb8d3aa504686cbaee5c9a5d3c22348
heajun jeong <heajun35@gmail.com>
2024-10-05 18:34:54 +0900

    [Feat] - 인기도 api  데이터 가져오는 부분 연결

--- src/util/maple/fetchCharacterInfo.ts
+++ src/util/maple/fetchCharacterInfo.ts
@@ -1,15 +1,18 @@
-import { basicCharacterInfo, statCharacterInfo } from "@/api/maple/axios";
+import {
+  basicCharacterInfo,
+  popularityCharacterInfo,
+  statCharacterInfo,
+} from "@/api/maple/axios";
 
 const fetchFullCharacterInfo = async (id: string) => {
   try {
-    // 두 개의 비동기 함수를 Promise 배열로 전달
-    const [basicInfo, statInfo] = await Promise.all([
+    const [basicInfo, statInfo, popularityInfo] = await Promise.all([
       basicCharacterInfo(id),
       statCharacterInfo(id),
+      popularityCharacterInfo(id),
     ]);
 
-    // 데이터를 객체 형태로 묶어서 반환
-    return { basicInfo, statInfo };
+    return { basicInfo, statInfo, popularityInfo };
   } catch (error) {
     console.error("Error fetching character info:", error);
     return Promise.reject(error);
