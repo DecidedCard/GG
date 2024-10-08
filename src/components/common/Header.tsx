1733c8fa1abfb95bb915f7800f33a4e51d79b272
heajun jeong <heajun35@gmail.com>
2024-10-08 20:05:32 +0900

    [Feat] - header 로고 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,11 +1,15 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import Logo from "@/../public/assets/maple/GG_logo.png";
 
 const Header = () => {
   return (
-    <header className="p-4 w-full border-b border-solid border-black">
-      <Link href={"/"}>
-        <p className="text-title/32px">Game-Information</p>
+    <header className="p-4 w-full h-28 border-b border-solid border-black">
+      <Link href={"/"} className="flex items-center gap-3 h-full">
+        <Image src={Logo} alt="로고" className="w-auto h-full" />
+        <p className="text-title/32px">Good_Game</p>
       </Link>
     </header>
   );
