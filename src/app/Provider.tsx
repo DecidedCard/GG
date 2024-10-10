48f7712ce0683608c3739ff1bbf891019d0e03d4
heajun jeong <heajun35@gmail.com>
2024-10-10 17:50:34 +0900

    [Feat] - next ui setting 및 tooltip 추가

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -3,12 +3,15 @@
 import React from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NextUIProvider } from "@nextui-org/system";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>{children}</NextUIProvider>
+    </QueryClientProvider>
   );
 };
 
