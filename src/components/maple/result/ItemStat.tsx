8a21cddd2e035a56fcb05d4e9196c5ac1dc8075b
heajun jeong <heajun35@gmail.com>
2024-10-11 16:53:54 +0900

    [Feat] - 아이템 스탯 title 정리

--- src/components/maple/result/ItemStat.tsx
+++ src/components/maple/result/ItemStat.tsx
@@ -40,24 +40,44 @@ const ItemStat = ({
   item: ItemEquipmentPreset;
   value: ItemStatValue; // 여전히 동일한 타입을 사용
 }) => {
-  console.log(value);
+  const title =
+    (value === "max_hp" && "최대 HP") ||
+    (value === "max_mp" && "최대 MP") ||
+    (value === "all_stat" && "올스탯") ||
+    (value === "armor" && "방어력") ||
+    (value === "attack_power" && "공격력") ||
+    (value === "magic_power" && "마력") ||
+    (value === "boss_damage" && "보스 몬스터 공격시 데미지") ||
+    (value === "ignore_monster_armor" && "몬스터 방어율 무시") ||
+    value.toUpperCase();
 
   return (
     <div className="flex gap-1">
       <p>
-        {value.toUpperCase()} : +
-        {item.item_total_option[value as ItemStatValueTotal]}
+        {title} : +{item.item_total_option[value as ItemStatValueTotal]}
       </p>
-      <p>
-        ({item.item_base_option[value as ItemStatValueBase]}
-        {item.item_add_option[value as ItemStatValueAdd] !== "0" &&
-          `+ ${item.item_add_option[value as ItemStatValueAdd]}`}
-        {item.item_etc_option[value as ItemStatValueEtc] !== "0" &&
-          `+ ${item.item_etc_option[value as ItemStatValueEtc]}`}
-        {item.item_starforce_option[value as ItemStatValueEtc] !== "0" &&
-          `+ ${item.item_starforce_option[value as ItemStatValueEtc]}`}
-        )
-      </p>
+      {(item.item_add_option[value as ItemStatValueAdd] !== "0" ||
+        item.item_etc_option[value as ItemStatValueEtc] !== "0" ||
+        item.item_starforce_option[value as ItemStatValueEtc] !== "0") && (
+        <p>
+          (<span>{item.item_base_option[value as ItemStatValueBase]}</span>
+          <span className="text-green-400">
+            {item.item_add_option[value as ItemStatValueAdd] !== "0" &&
+              `+ ${item.item_add_option[value as ItemStatValueAdd]}`}
+          </span>
+          <span className="text-purple-400">
+            {item.item_etc_option[value as ItemStatValueEtc] &&
+              item.item_etc_option[value as ItemStatValueEtc] !== "0" &&
+              `+ ${item.item_etc_option[value as ItemStatValueEtc]}`}
+          </span>
+          <span className="text-yellow-400">
+            {item.item_starforce_option[value as ItemStatValueEtc] &&
+              item.item_starforce_option[value as ItemStatValueEtc] !== "0" &&
+              `+ ${item.item_starforce_option[value as ItemStatValueEtc]}`}
+          </span>
+          )
+        </p>
+      )}
     </div>
   );
 };
