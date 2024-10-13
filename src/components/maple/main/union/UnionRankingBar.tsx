419e50347ef9e9acbc13fa192f2ae3d4d765649b
DecidedCard <qube7089@naver.com>
2024-10-13 18:05:36 +0900

    [Style] - 랭킹 바 css수정

--- src/components/maple/main/union/UnionRankingBar.tsx
+++ src/components/maple/main/union/UnionRankingBar.tsx
@@ -5,7 +5,7 @@ import type { UnionRanking } from "@/types/maple/mapleApi";
 
 const UnionRankingBar = ({ item }: { item: UnionRanking }) => {
   return (
-    <div className="flex justify-between py-2 px-3 w-full text-body/14px bg-primary-200 rounded">
+    <div className="flex justify-between pt-2 px-3 w-full text-body/14px border-t border-solid border-text-100 rounded">
       <div className="flex justify-between w-40">
         <p>{item.ranking}</p>
         <Link href={`/maple/result?character_name=${item.character_name}`}>
