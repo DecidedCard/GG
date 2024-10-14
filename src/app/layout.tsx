cea4371c735bdd96a4022680ee4f94198e967d8a
heajun jeong <heajun35@gmail.com>
2024-10-15 00:41:45 +0900

    [Feat] - 메타 데이터 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,18 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "GG",
   description: "good-game information",
+  keywords: ["게임", "굿게임", "rntrpdla", "good-game", "굿-게임", "rnt-rpdla"],
+  openGraph: {
+    siteName: "Good-Game",
+    title: "Good-Game",
+    description: "게임 캐릭터를 검색하여 캐릭터 정보를 확인해보세요!",
+    url: "https://gg-ashen-five.vercel.app/",
+    locale: "ko_KR",
+    type: "website",
+    images: {
+      url: "../../public/assets/GG_logo.png",
+    },
+  },
 };
 
 export default function RootLayout({
