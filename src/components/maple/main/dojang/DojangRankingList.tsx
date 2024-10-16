e83738957abeea4dd73993aeb88227f0afec46fa
heajun jeong <heajun35@gmail.com>
2024-10-16 20:39:07 +0900

    [Feat] - 랭킹 페이지 더보기 이동 구현

--- src/components/maple/main/dojang/DojangRankingList.tsx
+++ src/components/maple/main/dojang/DojangRankingList.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import Link from "next/link";
 
 import DojangRankingBar from "./DojangRankingBar";
+import DojangRankingTitle from "./DojangRankingTitle";
+import ArrowRoundForward from "@/assets/ArrowRoundForward";
 
 import type { DojangRanking } from "@/types/maple/mapleApi";
-import DojangRankingTitle from "./DojangRankingTitle";
 
 const DojangRankingList = ({ ranking }: { ranking: DojangRanking[] }) => {
   return (
     <div className="flex flex-col gap-2 p-3 bg-bg-300 text-text-100">
-      <h3 className="text-title/24px">TODAY 무릉도장 랭킹</h3>
+      <div className="flex justify-between items-center">
+        <h3 className="text-title/24px">TODAY 무릉도장 랭킹</h3>
+        <Link
+          href={"/maple/ranking/union"}
+          className="flex items-center gap-1 text-text-200 cursor-pointer"
+        >
+          더보기 <ArrowRoundForward />
+        </Link>
+      </div>
       <DojangRankingTitle />
       {ranking.slice(0, 10).map((item) => (
         <DojangRankingBar key={item.ranking} item={item} />
