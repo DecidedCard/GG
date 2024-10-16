b182ad72d65c0f5053f1a29c1947cfaef94e8d3f
heajun jeong <heajun35@gmail.com>
2024-10-16 22:20:13 +0900

    [Feat] - 랭킹페이지 기본 ui 작성

--- src/app/maple/ranking/total/page.tsx
+++ src/app/maple/ranking/total/page.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-const TotalRanking = () => {
-  return <div>TotalRanking</div>;
+import { overallRanking } from "@/api/maple/fetch";
+
+const TotalRanking = async () => {
+  const { ranking } = await overallRanking();
+  const contents = [...ranking.slice(0, 20)];
+  console.log(contents);
+  return (
+    <main className="p-5 mx-auto w-[1280px] text-text-100">
+      <h2>TODAY 일반월드 랭킹</h2>
+      <div className="flex flex-col gap-4 p-5 bg-bg-200">
+        {contents.map((item) => (
+          <ol
+            key={item.ranking}
+            className="flex justify-between items-center px-4"
+          >
+            <div className="flex gap-4 text-center">
+              <p className="w-12">{item.ranking}</p>
+              <p className="w-28">{item.character_name}</p>
+              <p className="w-10">{item.character_level}</p>
+              <p className="w-28">
+                {item.sub_class_name ? item.sub_class_name : item.class_name}
+              </p>
+            </div>
+            <div className="flex gap-4">
+              <p>{item.character_popularity}</p>
+              {item.character_guildname && <p>{item.character_guildname}</p>}
+            </div>
+          </ol>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default TotalRanking;
