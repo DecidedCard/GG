a3186ffbf2a26cdc18296bfabecddb60a55c3e6e
heajun jeong <heajun35@gmail.com>
2024-10-16 20:29:13 +0900

    [Feat] - 화살표 아이콘 추가

---
+++ src/assets/ArrowRoundForward.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { IoMdArrowRoundForward } from "react-icons/io";
+
+const ArrowRoundBack = ({ className }: { className: string }) => {
+  return <IoMdArrowRoundForward className={className} />;
+};
+
+export default ArrowRoundBack;
