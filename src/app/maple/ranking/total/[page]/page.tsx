163e86d0053e2ae99e42042ca35a2c4b56dcbcd4
heajun jeong <heajun35@gmail.com>
2024-10-18 15:49:00 +0900

    [Feat] - 컴포넌트 분리

--- src/app/maple/ranking/total/[page]/page.tsx
+++ src/app/maple/ranking/total/[page]/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import { overallRanking } from "@/api/maple/fetch";
+
 import PageButton from "@/components/maple/ranking/PageButton";
+import LevelRanking from "@/components/maple/ranking/LevelRanking";
 
 const TotalRanking = async ({ params }: { params: { page: number } }) => {
   const { ranking } = await overallRanking();
@@ -11,32 +13,7 @@ const TotalRanking = async ({ params }: { params: { page: number } }) => {
 
   return (
     <main className="flex flex-col gap-5 p-5 mx-auto w-[1280px] text-text-100">
-      <h2 className="text-title/28px">TODAY 일반월드 랭킹</h2>
-      <div className="flex flex-col gap-4 p-5 bg-bg-200">
-        {contents.map((item) => (
-          <ol
-            key={item.ranking}
-            className="flex justify-between items-center px-4"
-          >
-            <div className="flex gap-4 text-center">
-              <p className="w-12">{item.ranking}</p>
-              <p className="w-28">{item.character_name}</p>
-              <p className="w-10">{item.character_level}</p>
-              <p className="w-28">
-                {item.sub_class_name ? item.sub_class_name : item.class_name}
-              </p>
-            </div>
-            <div className="flex gap-4 text-center">
-              <p className="w-20">{item.character_popularity}</p>
-              <p className="w-20">
-                {item.character_guildname
-                  ? item.character_guildname
-                  : "길드없음"}
-              </p>
-            </div>
-          </ol>
-        ))}
-      </div>
+      <LevelRanking contents={contents} text="TODAY 일반월드 랭킹" />
       <PageButton length={ranking.length} page={params.page} />
     </main>
   );
