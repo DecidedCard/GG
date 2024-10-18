b9dbdc1a9d12d0bf5ec5b49a2498a091011d02bc
heajun jeong <heajun35@gmail.com>
2024-10-18 15:53:16 +0900

    [Feat] - 페이지 이동 작성

--- src/components/maple/main/dojang/DojangRankingList.tsx
+++ src/components/maple/main/dojang/DojangRankingList.tsx
@@ -13,7 +13,7 @@ const DojangRankingList = ({ ranking }: { ranking: DojangRanking[] }) => {
       <div className="flex justify-between items-center">
         <h3 className="text-title/24px">TODAY 무릉도장 랭킹</h3>
         <Link
-          href={"/maple/ranking/dojang"}
+          href={"/maple/ranking/dojang/1"}
           className="flex items-center gap-1 text-text-200 cursor-pointer"
         >
           더보기 <ArrowRoundForward />
