c35e2f97b0bd0b4d7f7a1d7912dd5b43c9e7d497
heajun jeong <heajun35@gmail.com>
2024-10-21 19:25:34 +0900

    [Feat] - 즐겨찾기 버튼 컴포넌트 구현

---
+++ src/components/maple/result/FavoriteCheck.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import { MdOutlineStar } from "react-icons/md";
+import { MdOutlineStarBorder } from "react-icons/md";
+
+const FavoriteCheck = ({ character }: { character: string }) => {
+  const [characterName, setCharacterName] = useState<string[]>([]);
+  const characterCheck = characterName.find((i) => i === character);
+
+  useEffect(() => {
+    const item = localStorage.getItem("favorite");
+
+    if (item) {
+      setCharacterName(JSON.parse(item));
+    }
+  }, []);
+
+  const onClickAddFavorite = () => {
+    const check = [...characterName, character];
+    setCharacterName(check);
+    localStorage.setItem(
+      "favorite",
+      JSON.stringify([...characterName, character])
+    );
+  };
+
+  const onClickDeleteFavorite = () => {
+    const index = characterName.indexOf(character);
+
+    const check = [...characterName];
+    check.splice(index, 1);
+
+    setCharacterName(check);
+    localStorage.setItem("favorite", JSON.stringify(check));
+  };
+
+  return characterCheck ? (
+    <MdOutlineStar
+      onClick={onClickDeleteFavorite}
+      className="absolute top-4 right-4 z-10 w-10 h-10 text-primary-100 cursor-pointer"
+    />
+  ) : (
+    <MdOutlineStarBorder
+      onClick={onClickAddFavorite}
+      className="absolute top-4 right-4 z-10 w-10 h-10 text-primary-100 cursor-pointer"
+    />
+  );
+};
+
+export default FavoriteCheck;
