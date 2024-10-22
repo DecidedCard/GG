8f7e94390bfbf73158541c8cd30210a0fef3f874
heajun jeong <heajun35@gmail.com>
2024-10-22 20:38:19 +0900

    [Feat] - 반응형 디자인 중 호버 효과 적용이 안되서 popover로 변경

--- src/components/maple/result/ChatacterInfo.tsx
+++ src/components/maple/result/ChatacterInfo.tsx
@@ -31,7 +31,7 @@ const CharacterInfo = () => {
 
   return (
     query.data && (
-      <div className="relative flex flex-col gap-4 mt-5 mx-auto w-[1280px] h-[3000px] text-text-100 lg:w-full md:w-[625px] sm:w-[450px] sm:h-[5000px]">
+      <div className="relative flex flex-col gap-4 mt-5 mx-auto w-[1280px] h-[3000px] text-text-100 lg:w-full md:w-[625px] sm:w-[450px] sm:h-[6000px]">
         <Link href={"/maple"} className="absolute top-4 left-4 z-20">
           <ArrowRoundBack className="w-9 h-9 text-primary-200 cursor-pointer sm:w-6 sm:h-6" />
         </Link>
--- src/components/maple/result/item/ItemCard.tsx
+++ src/components/maple/result/item/ItemCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import { Tooltip } from "@nextui-org/tooltip";
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/popover";
 
 import ItemDetail from "./ItemDetail";
 
@@ -24,67 +24,72 @@ const ItemCard = ({ item }: { item: ItemEquipmentPreset }) => {
     (item.additional_potential_option_grade === "레전드리" && "text-green-300");
 
   return (
-    <Tooltip content={<ItemDetail item={item} />}>
-      <ul
-        key={item.item_name}
-        className="flex flex-col justify-center w-full h-40 bg-bg-100 rounded-lg"
-      >
-        <div className="flex gap-3 p-3">
-          <Image
-            src={item.item_icon}
-            alt="아이템"
-            width={100}
-            height={100}
-            className="my-auto w-8 h-fit"
-          />
-          <div className="flex flex-col">
-            <p>{item.item_equipment_part}</p>
-            <p>{item.item_name}</p>
-            {item.starforce !== "0" && <p>⭐{item.starforce}</p>}
-            {item.special_ring_level !== 0 && (
-              <p>Lv. {item.special_ring_level}</p>
-            )}
-          </div>
-        </div>
-        {item.potential_option_grade && (
-          <div
-            className={`flex justify-between px-3 py-2 text-body/14px ${potentialColor}`}
-          >
-            <span className="w-10 text-center">잠재</span>
-            <div className="flex gap-1">
-              {item.potential_option_1 && (
-                <p>{formatItemStat(item.potential_option_1)}</p>
-              )}
-              {item.potential_option_1 && (
-                <p>{formatItemStat(item.potential_option_2)}</p>
-              )}
-              {item.potential_option_1 && (
-                <p>{formatItemStat(item.potential_option_3)}</p>
+    <Popover placement="top" showArrow>
+      <PopoverTrigger>
+        <ul
+          key={item.item_name}
+          className="flex flex-col justify-center w-full h-40 bg-bg-100 rounded-lg"
+        >
+          <div className="flex gap-3 p-3">
+            <Image
+              src={item.item_icon}
+              alt="아이템"
+              width={100}
+              height={100}
+              className="my-auto w-8 h-fit"
+            />
+            <div className="flex flex-col">
+              <p>{item.item_equipment_part}</p>
+              <p>{item.item_name}</p>
+              {item.starforce !== "0" && <p>⭐{item.starforce}</p>}
+              {item.special_ring_level !== 0 && (
+                <p>Lv. {item.special_ring_level}</p>
               )}
             </div>
           </div>
-        )}
+          {item.potential_option_grade && (
+            <div
+              className={`flex justify-between px-3 py-2 text-body/14px ${potentialColor}`}
+            >
+              <span className="w-10 text-center">잠재</span>
+              <div className="flex gap-1">
+                {item.potential_option_1 && (
+                  <p>{formatItemStat(item.potential_option_1)}</p>
+                )}
+                {item.potential_option_1 && (
+                  <p>{formatItemStat(item.potential_option_2)}</p>
+                )}
+                {item.potential_option_1 && (
+                  <p>{formatItemStat(item.potential_option_3)}</p>
+                )}
+              </div>
+            </div>
+          )}
 
-        {item.additional_potential_option_grade && (
-          <div
-            className={`flex justify-between px-3 py-2 text-body/14px ${additionalColor}`}
-          >
-            <span className="w-10 text-center">에디</span>
-            <div className="flex gap-1">
-              {item.additional_potential_option_1 && (
-                <p>{formatItemStat(item.additional_potential_option_1)}</p>
-              )}
-              {item.additional_potential_option_2 && (
-                <p>{formatItemStat(item.additional_potential_option_2)}</p>
-              )}
-              {item.additional_potential_option_3 && (
-                <p>{formatItemStat(item.additional_potential_option_3)}</p>
-              )}
+          {item.additional_potential_option_grade && (
+            <div
+              className={`flex justify-between px-3 py-2 text-body/14px ${additionalColor}`}
+            >
+              <span className="w-10 text-center">에디</span>
+              <div className="flex gap-1">
+                {item.additional_potential_option_1 && (
+                  <p>{formatItemStat(item.additional_potential_option_1)}</p>
+                )}
+                {item.additional_potential_option_2 && (
+                  <p>{formatItemStat(item.additional_potential_option_2)}</p>
+                )}
+                {item.additional_potential_option_3 && (
+                  <p>{formatItemStat(item.additional_potential_option_3)}</p>
+                )}
+              </div>
             </div>
-          </div>
-        )}
-      </ul>
-    </Tooltip>
+          )}
+        </ul>
+      </PopoverTrigger>
+      <PopoverContent>
+        <ItemDetail item={item} />
+      </PopoverContent>
+    </Popover>
   );
 };
 
