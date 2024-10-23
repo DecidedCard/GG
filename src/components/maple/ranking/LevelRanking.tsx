4e5f0ce887e6dbf87fd074b31af705e716c913b1
heajun jeong <heajun35@gmail.com>
2024-10-23 18:27:36 +0900

    [Style] - 메이플 랭킹 페이지 반응형 구현 및 기존 반응형 디자인 수정

--- src/components/maple/ranking/LevelRanking.tsx
+++ src/components/maple/ranking/LevelRanking.tsx
@@ -14,13 +14,13 @@ const LevelRanking = ({
 }) => {
   return (
     <>
-      <h2 className="flex justify-between text-title/28px">
+      <h2 className="flex justify-between text-title/28px sm:text-title/20px">
         <Link href={"/maple"}>
           <ArrowRoundBack />
         </Link>
         {text}
       </h2>
-      <div className="flex flex-col gap-4 p-5 bg-bg-200">
+      <div className="flex flex-col gap-4 p-5 bg-bg-200 md:text-body/14px sm:p-2 sm:min-w-80 sm:text-body/8px">
         {contents.map((item) => (
           <Link
             href={`/maple/result?character_name=${item.character_name}`}
@@ -31,16 +31,18 @@ const LevelRanking = ({
               className="flex justify-between items-center px-4"
             >
               <div className="flex gap-4 text-center">
-                <p className="w-12">{item.ranking}</p>
-                <p className="w-28">{item.character_name}</p>
-                <p className="w-10">{item.character_level}</p>
-                <p className="w-28">
+                <p className="w-12 md:w-8 sm:w-4">{item.ranking}</p>
+                <p className="w-28 md:w-18 sm:w-14">{item.character_name}</p>
+                <p className="w-10 md:w-8 sm:w-4">{item.character_level}</p>
+                <p className="w-28 md:w-18 sm:w-14">
                   {item.sub_class_name ? item.sub_class_name : item.class_name}
                 </p>
               </div>
               <div className="flex gap-4 text-center">
-                <p className="w-20">{item.character_popularity}</p>
-                <p className="w-20">
+                <p className="w-20 md:w-10 sm:w-6">
+                  {item.character_popularity}
+                </p>
+                <p className="w-20 md:w-16 sm:w-10">
                   {item.character_guildname
                     ? item.character_guildname
                     : "길드없음"}
