7669592bfb0e0a31aa3a9570ffffa5ed6622f872
heajun jeong <heajun35@gmail.com>
2024-10-25 17:20:17 +0900

    [Style] - 레이아웃에 걸리는 css 수정

--- src/components/common/ErrorModal.tsx
+++ src/components/common/ErrorModal.tsx
@@ -8,7 +8,7 @@ const ErrorModal = ({ children }: { children: React.ReactNode }) => {
   const { errObj } = useErrorModalStore();
 
   return (
-    <div>
+    <div className="h-full">
       {children}
       {errObj.isError && (
         <div className="fixed top-0 right-0 bottom-0 left-0 flex justify-center items-center bg-black bg-opacity-80">
