472d75ca56da8c1a0dbd59dbed10d59ab244b73d
heajun jeong <heajun35@gmail.com>
2024-10-27 22:14:15 +0900

    [Feat] - 심볼 디테일 작성 및 popover 작성

--- src/components/maple/result/symbol/SymbolCard.tsx
+++ src/components/maple/result/symbol/SymbolCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import type { Symbol } from "@/types/maple/mapleApi";
 import Image from "next/image";
+import CustomPopover from "@/components/common/CustomPopover";
+import SymbolDetail from "./SymbolDetail";
 
 const SymbolCard = ({ item }: { item: Symbol }) => {
   const stat =
@@ -12,31 +14,33 @@ const SymbolCard = ({ item }: { item: Symbol }) => {
     (item.symbol_str !== "0" && item.symbol_str);
 
   return (
-    <div className="flex flex-col items-center gap-2 p-4 w-72 border border-solid border-text-100 rounded-md">
-      <p>{item.symbol_name}</p>
-      <hr className="w-full border-text-200" />
-      <div className="flex justify-around items-center w-full">
-        <Image
-          src={item.symbol_icon}
-          alt={item.symbol_name}
-          width={50}
-          height={50}
-          className="w-[50px] h-[50px]"
-        />
-        <div className="flex gap-5 w-40 text-center">
-          <div className="flex flex-col items-end w-24">
-            <p>{item.symbol_name.slice(0, 3)}포스:</p>
-            <p>주스탯:</p>
-            <p>레벨:</p>
-          </div>
-          <div className="flex flex-col items-end">
-            <p>{item.symbol_force}</p>
-            <p>{stat}</p>
-            <p>{item.symbol_level}</p>
+    <CustomPopover popoverContents={<SymbolDetail item={item} />}>
+      <div className="flex flex-col items-center gap-2 p-4 w-72 border border-solid border-text-100 rounded-md">
+        <p>{item.symbol_name}</p>
+        <hr className="w-full border-text-200" />
+        <div className="flex justify-around items-center w-full">
+          <Image
+            src={item.symbol_icon}
+            alt={item.symbol_name}
+            width={50}
+            height={50}
+            className="w-[50px] h-[50px]"
+          />
+          <div className="flex gap-5 w-40 text-center">
+            <div className="flex flex-col items-end w-24">
+              <p>{item.symbol_name.slice(0, 3)}포스:</p>
+              <p>주스탯:</p>
+              <p>레벨:</p>
+            </div>
+            <div className="flex flex-col items-end">
+              <p>{item.symbol_force}</p>
+              <p>{stat}</p>
+              <p>{item.symbol_level}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </CustomPopover>
   );
 };
 
---
+++ src/components/maple/result/symbol/SymbolDetail.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import type { Symbol } from "@/types/maple/mapleApi";
+import Image from "next/image";
+
+const SymbolDetail = ({ item }: { item: Symbol }) => {
+  return (
+    <div className="flex flex-col gap-2 p-6 w-96 bg-bg-300 text-white rounded-lg">
+      <p className="mx-auto text-title/20px">{item.symbol_name}</p>
+      <hr className="w-full border-dashed border-text-200" />
+      <div className="flex items-center gap-5 px-4">
+        <Image
+          src={item.symbol_icon}
+          alt={item.symbol_name}
+          width={30}
+          height={30}
+        />
+        <p className="text-text-200">REQ LEV: 200</p>
+      </div>
+      <hr className="w-full border-dashed border-text-200" />
+      <div className="flex flex-col px-4">
+        <p>성장레벨: {item.symbol_level}</p>
+        <p>
+          {item.symbol_growth_count !== 0
+            ? item.symbol_growth_count + "/" + item.symbol_require_growth_count
+            : "MAX"}
+        </p>
+        {item.symbol_str !== "0" && <p>STR: +{item.symbol_str}</p>}
+        {item.symbol_dex !== "0" && <p>DEX: +{item.symbol_dex}</p>}
+        {item.symbol_int !== "0" && <p>INT: +{item.symbol_int}</p>}
+        {item.symbol_luk !== "0" && <p>LUK: +{item.symbol_luk}</p>}
+        {item.symbol_hp !== "0" && <p>HP: +{item.symbol_hp}</p>}
+        <p>
+          {item.symbol_name.slice(0, 3)}포스: {item.symbol_force}
+        </p>
+      </div>
+      <hr className="w-full border-dashed border-text-200" />
+      <p className="px-4 whitespace-break-spaces">{item.symbol_description}</p>
+    </div>
+  );
+};
+
+export default SymbolDetail;
