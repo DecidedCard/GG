dcb57f931639fca18a71f509b4c290fb1592053d
heajun jeong <heajun35@gmail.com>
2024-11-10 14:35:53 +0900

    [Feat] - 결과/스탯 및 아이템 페이지 컴포넌트 분리

---
+++ src/components/maple/result/characterInfo/StatAndItemInfo.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useSearchParams } from "next/navigation";
+
+import StatInfo from "./StatInfo";
+import ItemList from "./item/ItemList";
+
+import { useCharacterQuery } from "@/hooks/maple/useQuery";
+
+const StatAndItemInfo = () => {
+  const params = useSearchParams();
+
+  const name = params.get("character_name");
+
+  const { data } = useCharacterQuery(name, "stat");
+
+  return (
+    data.statInfo &&
+    data.itemInfo && (
+      <div className="flex gap-3 sm:flex-col">
+        <StatInfo info={data.statInfo} />
+        <ItemList item={data.itemInfo} />
+      </div>
+    )
+  );
+};
+
+export default StatAndItemInfo;
