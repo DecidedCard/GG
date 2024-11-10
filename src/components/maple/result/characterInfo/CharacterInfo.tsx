0ea6ed094341f7c6b95400a106e1e42ea40c6ad8
heajun jeong <heajun35@gmail.com>
2024-11-10 22:39:57 +0900

    [Update] - 조건 변경

--- src/components/maple/result/characterInfo/CharacterInfo.tsx
+++ src/components/maple/result/characterInfo/CharacterInfo.tsx
@@ -14,15 +14,15 @@ const CharacterInfo = ({ children }: { children: React.ReactNode }) => {
   const { query, onClickCharacterInfoSet } = useResult();
 
   return (
-    query.data && (
+    query.data.basicInfo && (
       <div className="relative flex flex-col gap-4 mt-5 mx-auto w-[1280px] h-[3000px] text-text-100 lg:w-full md:w-full md:h-[4500px] sm:w-full sm:h-[10000px]">
         <Link href={"/maple"} className="absolute top-4 left-4 z-20">
           <ArrowRoundBack className="w-9 h-9 text-primary-200 cursor-pointer sm:w-6 sm:h-6" />
         </Link>
         <BasicInfo
-          info={query.data.basicInfo!}
-          popularity={query.data.popularityInfo!.popularity}
-          cashItem={query.data.cashItemInfo!}
+          info={query.data.basicInfo}
+          popularity={query.data.popularityInfo.popularity}
+          cashItem={query.data.cashItemInfo}
         />
         <InfoNav onClickCharacterInfoSet={onClickCharacterInfoSet} />
         {children}
