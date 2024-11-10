95fd1ff935a1894ce785375d97c8ae1d0cd7fcd8
heajun jeong <heajun35@gmail.com>
2024-11-10 14:31:10 +0900

    [Feat] - 결과/스킬 페이지 컴포넌트 분리

--- src/app/maple/result/skill/page.tsx
+++ src/app/maple/result/skill/page.tsx
@@ -1,30 +1,15 @@
 "use client";
 
 import React, { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 
 import { Spinner } from "@nextui-org/spinner";
 
-import LinkSkillList from "@/components/maple/result/skill/LinkSkillList";
-import SkillList from "@/components/maple/result/skill/SkillList";
-import SymbolList from "@/components/maple/result/symbol/SymbolList";
-import { useCharacterQuery } from "@/hooks/maple/useQuery";
+import SkillInfo from "@/components/maple/result/skill/SkillInfo";
 
 const SkillPage = () => {
-  const params = useSearchParams();
-
-  const name = params.get("character_name");
-
-  const { data } = useCharacterQuery(name, "skill");
-
   return (
     <Suspense fallback={<Spinner />}>
-      <div className="flex flex-col gap-5 w-[1280px] md:w-full sm:w-full">
-        <SkillList skillInfo={data.sixthSkillInfo!} />
-        <SkillList skillInfo={data.fifthSkillInfo!} />
-        <SymbolList item={data.symbolInfo!} />
-        <LinkSkillList item={data.linkSkillInfo!} />
-      </div>
+      <SkillInfo />
     </Suspense>
   );
 };
---
+++ src/components/maple/result/skill/SkillInfo.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { useSearchParams } from "next/navigation";
+
+import { useCharacterQuery } from "@/hooks/maple/useQuery";
+
+import SkillList from "./SkillList";
+import SymbolList from "../symbol/SymbolList";
+import LinkSkillList from "./LinkSkillList";
+
+const SkillInfo = () => {
+  const params = useSearchParams();
+
+  const name = params.get("character_name");
+
+  const { data } = useCharacterQuery(name, "skill");
+
+  return (
+    <div className="flex flex-col gap-5 w-[1280px] md:w-full sm:w-full">
+      <SkillList skillInfo={data.sixthSkillInfo!} />
+      <SkillList skillInfo={data.fifthSkillInfo!} />
+      <SymbolList item={data.symbolInfo!} />
+      <LinkSkillList item={data.linkSkillInfo!} />
+    </div>
+  );
+};
+
+export default SkillInfo;
