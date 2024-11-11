d2dd0f3a419f2a7cb4f587968425f0466c2be383
heajun jeong <heajun35@gmail.com>
2024-11-11 13:37:09 +0900

    [refactor] - 에러 작성

--- src/hooks/maple/useQuery.ts
+++ src/hooks/maple/useQuery.ts
@@ -12,7 +12,7 @@ export const useBasicCharacterQuery = (id: string | null) => {
   const { setIsError, setReset } = useErrorModalStore();
   const navigation = useRouter();
 
-  const { data, isError, isFetching } = useSuspenseQuery({
+  const { data, isError, error } = useSuspenseQuery({
     queryKey: [QUERY_KEY.characterInfo, id],
     queryFn: () => fetchCharacterData(id!),
     retry: 0,
@@ -28,13 +28,13 @@ export const useBasicCharacterQuery = (id: string | null) => {
 
       setIsError({
         isError: true,
-        comment: "캐릭터 검색 중 오류가 발생하였습니다.",
+        comment: `${error}`,
         onClickFn: onClickHandler,
       });
     }
-  }, [isError, setIsError, setReset, navigation]);
+  }, [isError, error, setIsError, setReset, navigation]);
 
-  return { isFetching, isError, data };
+  return { isError, data };
 };
 
 export const useCharacterQuery = (
@@ -44,7 +44,7 @@ export const useCharacterQuery = (
   const { setIsError, setReset } = useErrorModalStore();
   const navigation = useRouter();
 
-  const { data, isError, isFetching } = useSuspenseQuery({
+  const { data, isError, error } = useSuspenseQuery({
     queryKey: [QUERY_KEY.characterInfo, id, type],
     queryFn: () => fetchCharacterData(id!, type),
     retry: 0,
@@ -60,11 +60,11 @@ export const useCharacterQuery = (
 
       setIsError({
         isError: true,
-        comment: `${type}을 검색하는 중에 오류가 발생하였습니다.`,
+        comment: `${error}`,
         onClickFn: onClickHandler,
       });
     }
-  }, [type, isError, setIsError, setReset, navigation]);
+  }, [type, isError, error, setIsError, setReset, navigation]);
 
-  return { isFetching, isError, data };
+  return { isError, data };
 };
--- src/util/maple/fetchCharacterData.ts
+++ src/util/maple/fetchCharacterData.ts
@@ -17,41 +17,46 @@ export const fetchCharacterData = async (
   name: string,
   type?: "stat" | "skill" | "union"
 ) => {
-  const { ocid } = await getCharacterId(name);
+  try {
+    const { ocid } = await getCharacterId(name);
 
-  switch (type) {
-    case "stat":
-      const [statInfo, itemInfo] = await Promise.all([
-        statCharacterInfo(ocid),
-        itemCharacterInfo(ocid),
-      ]);
-      return { statInfo, itemInfo };
-
-    case "skill":
-      const [fifthSkillInfo, sixthSkillInfo, symbolInfo, linkSkillInfo] =
-        await Promise.all([
-          SkillCharacterInfo(ocid, 5),
-          SkillCharacterInfo(ocid, 6),
-          symbolCharacterInfo(ocid),
-          linkSkillCharacterInfo(ocid),
+    switch (type) {
+      case "stat":
+        const [statInfo, itemInfo] = await Promise.all([
+          statCharacterInfo(ocid),
+          itemCharacterInfo(ocid),
         ]);
-      return { fifthSkillInfo, sixthSkillInfo, symbolInfo, linkSkillInfo };
+        return { statInfo, itemInfo };
 
-    case "union":
-      const [unionCharacterInfo, unionArtifactInfo, unionRaiderInfo] =
-        await Promise.all([
-          unionCharacterCharacterInfo(ocid),
-          unionArtifactCharacterCharacterInfo(ocid),
-          unionRaiderCharacterCharacterInfo(ocid),
+      case "skill":
+        const [fifthSkillInfo, sixthSkillInfo, symbolInfo, linkSkillInfo] =
+          await Promise.all([
+            SkillCharacterInfo(ocid, 5),
+            SkillCharacterInfo(ocid, 6),
+            symbolCharacterInfo(ocid),
+            linkSkillCharacterInfo(ocid),
+          ]);
+        return { fifthSkillInfo, sixthSkillInfo, symbolInfo, linkSkillInfo };
+
+      case "union":
+        const [unionCharacterInfo, unionArtifactInfo, unionRaiderInfo] =
+          await Promise.all([
+            unionCharacterCharacterInfo(ocid),
+            unionArtifactCharacterCharacterInfo(ocid),
+            unionRaiderCharacterCharacterInfo(ocid),
+          ]);
+        return { unionCharacterInfo, unionArtifactInfo, unionRaiderInfo };
+
+      default:
+        const [basicInfo, cashItemInfo, popularityInfo] = await Promise.all([
+          basicCharacterInfo(ocid),
+          cashItemCharacterInfo(ocid),
+          popularityCharacterInfo(ocid),
         ]);
-      return { unionCharacterInfo, unionArtifactInfo, unionRaiderInfo };
-
-    default:
-      const [basicInfo, cashItemInfo, popularityInfo] = await Promise.all([
-        basicCharacterInfo(ocid),
-        cashItemCharacterInfo(ocid),
-        popularityCharacterInfo(ocid),
-      ]);
-      return { basicInfo, cashItemInfo, popularityInfo };
+        return { basicInfo, cashItemInfo, popularityInfo };
+    }
+  } catch (error) {
+    console.error("캐릭터 정보를 가져오는 도중 에러가 발생했습니다.", error);
+    return Promise.reject(error);
   }
 };
