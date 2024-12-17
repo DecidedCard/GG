94964445663ba713253be617b0c42787b23f712d
heajun jeong <heajun35@gmail.com>
2024-12-17 19:05:17 +0900

    [Feat] - 익셉셔널 강화 정보 추가가

--- src/components/maple/result/item/ItemPotential.tsx
+++ src/components/maple/result/item/ItemPotential.tsx
@@ -21,14 +21,14 @@ const ItemPotential = ({ item }: { item: ItemEquipmentPreset }) => {
         <div className="flex flex-col gap-1 py-2 px-4 w-full text-body/14px border-t border-solid border-gray-400">
           <p className="flex items-center gap-2">
             <span
-              className={`flex justify-center items-center w-5 h-5 rounded ${potentialBackgroundColor}`}
+              className={`flex justify-center items-center w-6 h-5 rounded ${potentialBackgroundColor}`}
             >
               {(item.potential_option_grade === "레어" && "R") ||
                 (item.potential_option_grade === "에픽" && "E") ||
                 (item.potential_option_grade === "유니크" && "U") ||
                 (item.potential_option_grade === "레전드리" && "L")}
             </span>
-            잠재옵션
+            잠재 옵션
           </p>
           <p>{item.potential_option_1}</p>
           <p>{item.potential_option_2}</p>
@@ -39,18 +39,32 @@ const ItemPotential = ({ item }: { item: ItemEquipmentPreset }) => {
         <div className="flex flex-col gap-1 py-2 px-4 w-full text-body/14px border-t border-solid border-gray-400">
           <p className="flex items-center gap-2">
             <span
-              className={`flex justify-center items-center w-5 h-5 rounded ${additionalPotentialBackgroundColor}`}
+              className={`flex justify-center items-center w-6 h-5 rounded ${additionalPotentialBackgroundColor}`}
             >
               {(item.additional_potential_option_grade === "레어" && "R") ||
                 (item.additional_potential_option_grade === "에픽" && "E") ||
                 (item.additional_potential_option_grade === "유니크" && "U") ||
                 (item.additional_potential_option_grade === "레전드리" && "L")}
             </span>
-            잠재옵션
+            에디셔널 잠재 옵션
           </p>
           <p>{item.additional_potential_option_1}</p>
           <p>{item.additional_potential_option_2}</p>
           <p>{item.additional_potential_option_3}</p>
+        </div>
+      )}
+      {item.item_exceptional_option.exceptional_upgrade && (
+        <div className="flex flex-col gap-1 py-2 px-4 w-full text-body/14px border-t border-solid border-gray-400">
+          <p className="flex items-center gap-2">
+            <span className="flex justify-center items-center w-6 h-5 rounded bg-accent-300">
+              EX
+            </span>
+            익셉셔널 강화
+          </p>
+          <p>올스탯 : +{item.item_exceptional_option.str}</p>
+          <p>최대 HP / 최대 MP : +{item.item_exceptional_option.max_hp}</p>
+          <p>공격력 / 마력 : +{item.item_exceptional_option.attack_power}</p>
+          <p>익셉셔널 강화 1회 적용 (최대 1회 강화 가능)</p>
         </div>
       )}
     </>
