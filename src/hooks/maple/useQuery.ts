972618a7a5b4f0c81a13821318090cd4ca0a1398
heajun jeong <heajun35@gmail.com>
2025-02-02 18:34:05 +0900

    [Delete] - 사용하지 않는 파일 및 코드 제거거

--- src/hooks/maple/useQuery.ts
+++ src/hooks/maple/useQuery.ts
@@ -8,33 +8,6 @@ import useErrorModalStore from "@/store/errorModalStore";
 import QUERY_KEY from "@/util/maple/QUERY_KEY";
 import { fetchCharacterData } from "@/util/maple/fetchCharacterData";
 
-export const useBasicCharacterQuery = (id: string | null) => {
-  const { setIsError, setReset } = useErrorModalStore();
-  const navigation = useRouter();
-
-  const { data, isError, error } = useSuspenseQuery({
-    queryKey: [QUERY_KEY.characterInfo, id],
-    queryFn: () => fetchCharacterData(id!),
-  });
-
-  useEffect(() => {
-    if (isError) {
-      const onClickHandler = () => {
-        setReset();
-        navigation.push("/maple");
-      };
-
-      setIsError({
-        isError: true,
-        comment: `${error}`,
-        onClickFn: onClickHandler,
-      });
-    }
-  }, [isError, error, setIsError, setReset, navigation]);
-
-  return { isError, data };
-};
-
 export const useCharacterQuery = (id: string) => {
   const { setIsError, setReset } = useErrorModalStore();
   const navigation = useRouter();
