463dc7701963ca072ab87236a987def0a1904374
heajun jeong <heajun35@gmail.com>
2025-02-03 19:37:56 +0900

    [Update] - query에 useEffect를 제거 하여 불필요한 렌더링 제거거

--- src/hooks/maple/useQuery.ts
+++ src/hooks/maple/useQuery.ts
@@ -1,36 +1,13 @@
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
 import { useSuspenseQuery } from "@tanstack/react-query";
-
-import useErrorModalStore from "@/store/errorModalStore";
 
 import QUERY_KEY from "@/util/maple/QUERY_KEY";
 import { fetchCharacterData } from "@/util/maple/fetchCharacterData";
 
 export const useCharacterQuery = (id: string) => {
-  const { setIsError, setReset } = useErrorModalStore();
-  const navigation = useRouter();
-
   const { data, isError, error } = useSuspenseQuery({
     queryKey: [QUERY_KEY.characterInfo, id],
     queryFn: () => fetchCharacterData(id!),
   });
 
-  useEffect(() => {
-    if (isError) {
-      const onClickHandler = () => {
-        setReset();
-        navigation.push("/maple");
-      };
-
-      setIsError({
-        isError: true,
-        comment: `${error}`,
-        onClickFn: onClickHandler,
-      });
-    }
-  }, [isError, error, setIsError, setReset, navigation]);
-
-  return { isError, data };
+  return { isError, error, data };
 };
