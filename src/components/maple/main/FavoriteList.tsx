41c85e097422db8e963779f04c96ff9e404ab7e6
heajun jeong <heajun35@gmail.com>
2025-02-17 17:21:21 +0900

    [Style] - css 수정

--- src/components/maple/main/FavoriteList.tsx
+++ src/components/maple/main/FavoriteList.tsx
@@ -16,20 +16,17 @@ const FavoriteList = () => {
 
   return (
     list.length !== 0 && (
-      <section className="flex flex-col gap-5 mx-auto mb-10 p-4 w-[700px] text-text-100 md:w-[550px] sm:w-[350px]">
-        <p className="text-body/22px">즐겨찾기</p>
-        <div className="flex flex-wrap gap-5">
-          {list.map((item) => (
-            <Link
-              key={item}
-              href={`/maple/result?character_name=${item}&type=stat`}
-            >
-              <ol className="py-2 w-36 border border-solid border-text-100 text-center rounded-lg">
-                {item}
-              </ol>
-            </Link>
-          ))}
-        </div>
+      <section className="flex flex-wrap gap-5 mx-auto mb-10 p-4 w-[700px] md:w-[550px] sm:w-[350px]">
+        {list.map((item) => (
+          <Link
+            key={item}
+            href={`/maple/result?character_name=${item}&type=stat`}
+          >
+            <ol className="py-2 w-36 border-2 border-solid border-primary-200 text-center rounded-lg">
+              {item}
+            </ol>
+          </Link>
+        ))}
       </section>
     )
   );
